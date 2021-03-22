9945c365c2a187cf080ff0957895dbbee4bdeb31
Jay Zhang <wangyoucao577@gmail.com>
2021-03-22 18:09:16 +0800

    feat: enable drag waypoints

--- src/plan.js
+++ src/plan.js
@@ -14,7 +14,7 @@
 				{color: 'white', opacity: 0.8, weight: 6},
 				{color: 'red', opacity: 1, weight: 2, dashArray: '7,12'}
 			],
-			draggableWaypoints: false,//true,
+			draggableWaypoints: true,
 			routeWhileDragging: false,
 			addWaypoints: true,
 			reverseWaypoints: false,
